01c93ea20f66cc04b60d9d0c2dfad38ec932f51b
DBNitro.net <ribas@dbnitro.net>
2025-08-26 17:21:43 +0200

    Add files via upload

---
+++ sql/ASM/ASM_VERIFY_DISKS_READ_AND_WRITE_VALUES.sql
@@ -0,0 +1,25 @@
+-- Author...........: Andre Augusto Ribas"
+-- SoftwareVersion..: 1.0.1"
+-- DateCreation.....: 20/02/2024
+-- DateModification.: 16/08/2024
+-- EMAIL............: ribas@dbnitro.net
+-- GitHub...........: https://github.com/DBNitro
+-- WEBSITE..........: http://dbnitro.net
+
+set pages 700 lines 700 timing on long 9999999 numwidth 20 heading on echo on verify on feedback on colsep '|'
+prompt ##############################################################
+prompt # ASM: VERIFY DISKS READ AND WRITE VALUES
+prompt ##############################################################
+SELECT SUBSTR(dgs.name,1,20) AS diskgroup
+  , SUBSTR(ds.name,1,20) AS asmdisk
+  , ds.mount_status
+  , ds.state
+  , ds.reads
+  , ds.writes
+  , ds.read_time
+  , ds.write_time
+  , bytes_read
+  , bytes_written
+FROM V$ASM_DISKGROUP_STAT dgs, V$ASM_DISK_STAT ds
+WHERE dgs.group_number = ds.group_number
+order by 1,2,3,4,5;
